44ae553a8ff94e52c8c5f7709b5989b08abeaa41
Nikakto <mcgish@gmail.com>
2018-11-04 14:35:20 +0300

    lab3 print results

--- lab_3.py
+++ lab_3.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from lab_2 import cross_db_query
 
+COST_BASE = 0
 COST_CROSS_DB = 5
 COST_CROSS_DB_PARTS = [2, 3, 2]
 COST_SIMPLE = [1, 3, 2]
@@ -15,16 +16,19 @@ INDEX = list(range(1, len(COST_SIMPLE) + 1, 1))
 MASK = np.array(INDEX)
 VARIANTS = list(itertools.product(INDEX, repeat=2))
 
-results = [
+results = np.array([
     {
-        'cost': np.ma.array(COST_SIMPLE, mask=MASK == driver).sum() + COST_CROSS_DB_MIN_COST[source-1],
+        'cost': np.ma.array(COST_SIMPLE, mask=MASK == driver).sum() + COST_CROSS_DB_MIN_COST[source-1] + (source != driver) * COST_BASE,
         'source': source,
         'driver': driver,
         'cross_db_driver': COST_CROSS_DB_MIN_DRIVER[source-1]
     } for source, driver in VARIANTS
-]
+]).reshape(3, 3)
 
-optimals = [min(row, key=lambda el: el['cost']) for row in np.array(results).reshape(3, 3)]
+print('\nМатрица. Строка - источник; Столбец - координатор:\n',
+      np.array([[result['cost'] for result in row] for row in results]))
+
+optimals = [min(row, key=lambda el: el['cost']) for row in results]
 for optimal in optimals:
     print('''
         Источник: {source};
